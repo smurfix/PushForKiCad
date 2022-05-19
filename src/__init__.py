d3ea27778a1312dc427443a7ad143577d3274562
Matthias Urlichs <matthias@urlichs.de>
2022-05-19 14:03:20 +0200

    print traceback

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,18 @@
+stderr=open("/dev/tty","w")
+
 try:
+    print("Loading",file=stderr)
+    import sys
+    sys.modules.pop("wx",None)
+    sys.modules.pop("wx.__version__",None)
     from .plugin import PushForKiCadPlugin
     plugin = PushForKiCadPlugin()
     plugin.register()
-except Exception as e:
+    print("OK",file=stderr)
+except BaseException as e:
     import logging
     root = logging.getLogger()
     root.debug(repr(e))
+    import traceback
+    traceback.print_exc(file=stderr)
+    raise
